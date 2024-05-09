82979b7d95b83c6cd302be337579955ca7bc214e
Sananesaw <145952788+Sananesaw@users.noreply.github.com>
2024-05-09 04:00:58 +0300

    Create dclink.js

---
+++ komutlar/dclink.js
@@ -0,0 +1,24 @@
+const {EmbedBuilder} = require("discord.js");
+
+exports.run = async (client, message, args) => {
+
+    const embed = new EmbedBuilder()
+    .setTitle("Sunucu Davet")
+    .setDescription(`**[KATILMAK İÇİN TIKLA](https://discord.gg/zZ4BY8uVZ4)**\n`)
+    .setColor("#007fff")
+    .setTimestamp()
+    return message.channel.send({embeds : [embed]});
+
+};
+
+module.exports.conf = {
+aliases: ['dc','link'],
+permLevel: 0, 
+kategori: 'Genel'
+};
+
+module.exports.help = {
+    name: 'dc',
+    description: 'Discord linki atar.',
+    usage: 'dc'
+};
