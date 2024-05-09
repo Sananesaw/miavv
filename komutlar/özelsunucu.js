9a32489d5c3a7ea4b6342888c1703a9348d9ae01
Sananesaw <145952788+Sananesaw@users.noreply.github.com>
2024-05-09 04:20:40 +0300

    Create özelsunucu.js

---
+++ komutlar/özelsunucu.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js')
+const ayarlar = require('../ayarlar.json') 
+
+exports.run = async (client, message, args) => {
+    var prefix = ayarlar.prefix;
+if (message.author.id != "420927049939681280") return message.reply('Bunu Sadece Sahibim Kullanabilir');
+let üyesayi = message.guild.memberCount;
+let botlar = message.guild.members.cache.filter(m => m.user.bot).size
+let kullanıcılar = üyesayi - botlar
+const embed = new Discord.EmbedBuilder()
+.setColor(Discord.Colors.Blue)
+.setTimestamp()
+.addFields([
+{ name: `Toplam Üye`, value: `**${üyesayi}**`, inline:true },
+{ name: `Kullanıcılar`, value: `**${kullanıcılar}**`, inline:true },
+{ name: `Botlar`, value: `**${botlar}**`, inline:true },
+{ name: `Üye Durumları`, value: `**${message.guild.members.cache.filter(o => o.presence?.status === "online").size}** Çevrimiçi\n**${message.guild.members.cache.filter(i => i.presence?.status === "idle").size}** Boşta\n**${message.guild.members.cache.filter(dnd => dnd.presence?.status === "dnd").size}** Rahatsız Etmeyin`, inline:true },
+])
+return message.channel.send({embeds: [embed]})
+}
+
+exports.conf = {
+aliases: [""]
+}
+
+exports.help = {
+name: "sunucubilgi"
+}
