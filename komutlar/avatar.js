97dcf4c6a6f1961774c55e9776389a0bbd0cbf58
Sananesaw <145952788+Sananesaw@users.noreply.github.com>
2024-05-09 03:34:08 +0300

    avatar.js

---
+++ komutlar/avatar.js
@@ -0,0 +1,23 @@
+const { EmbedBuilder } = require("discord.js");
+exports.run = async (client, message, args) => {
+
+    let user = message.mentions.users.first() || message.author;
+    const embed = new EmbedBuilder()
+    .setDescription(`**[PNG](${user.displayAvatarURL({ dynamic: true, size: 1024 }).replace("webp", "png")}) | [JPG](${user.displayAvatarURL({ dynamic: true, size: 1024 }).replace("webp", "jpg")}) | [WEBP](${user.displayAvatarURL({ dynamic: true, size: 1024 }).replace("webp", "webp")}) | [GIF](${user.displayAvatarURL({ dynamic: true, size: 1024 }).replace("webp", "gif")})**`)
+    .setImage(user.displayAvatarURL({ dynamic: true, size: 1024 }))
+    .setColor("#0099ff")
+    return message.reply({ embeds: [embed]}).catch(err => {})
+
+}
+
+module.exports.conf = {
+    aliases: ['avatar', 'pp'],
+    permLevel: 0,
+    kategori: 'Genel'
+};
+
+module.exports.help = {
+    name: 'avatar',
+    description: 'Kullanıcının avatarını gösterir.',
+    usage: 'avatar'
+};
