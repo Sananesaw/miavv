4da06ca7a6c4a9cf1d0861b66aa280ee902caf61
Sananesaw <145952788+Sananesaw@users.noreply.github.com>
2024-05-09 04:19:29 +0300

    Create özeldm.js

---
+++ komutlar/özeldm.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+exports.run = async (client, message, args) => {
+  if (!message.member.permissions.has("0x0000000000000008"))
+    return message.channel.send(
+      `> **Bu komutu kullanabilmek için "\`YÖNETİCİt\`" yetkisine sahip olmalısın.**`
+      
+    );
+  if (message.author.id != "420927049939681280") return message.reply('Bunu Sadece Sahibim Kullanabilir');
+
+  let mesaj = args[0];
+  if (!mesaj) return message.channel.send("> **Birşey Yazmalısınız**");
+
+  await message.delete();
+
+  const mesajat = new Discord.EmbedBuilder()
+    .setColor(Discord.Colors.Blue)
+    .setDescription(args.slice(0).join(" "));
+
+  message.guild.members.cache.map(async (user) => {
+    await user.send({ embeds: [mesajat] }).catch((e) => {});
+  });
+
+  await message.channel.send(`> **✅ Mesaj basariyla gonderildi.**`);
+};
+
+exports.conf = {
+  aliases: ["dm-duyuru"],
+};
+
+exports.help = {
+  name: "dm",
+};
