ffe86fd6b84761609177875b0fd4ed909085a0b3
Sananesaw <145952788+Sananesaw@users.noreply.github.com>
2024-05-09 04:20:51 +0300

    Create özelyaz.js

---
+++ komutlar/özelyaz.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+
+exports.run = (client, message, args) => {
+  let mesaj = args.slice(0).join(' ');
+if (message.author.id != "420927049939681280") return message.reply('Bunu Sadece Sahibim Kullanabilir');
+  message.delete();
+  message.channel.send(mesaj);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['say', 'söyle'],
+  permLevel: 5
+};
+
+exports.help = {
+  name: 'y',
+  description: 'İstediğiniz şeyi bota yazdırır.',
+  usage: 'yaz [yazdırmak istediğiniz şey]'
+};
