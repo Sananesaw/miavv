306690c7620f1186ce3ae5730dd69fc3239e49cc
Sananesaw <145952788+Sananesaw@users.noreply.github.com>
2024-05-09 03:57:35 +0300

    Create davet1.js

---
+++ komutlar/davet1.js
@@ -0,0 +1,31 @@
+const { EmbedBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle } = require("discord.js")
+module.exports.run= async(client, message, args) => {
+
+let link_button = new ActionRowBuilder().addComponents(
+new ButtonBuilder()
+.setLabel('Davet Et')
+.setStyle(ButtonStyle.Link)
+.setURL(`https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=0&scope=bot%20applications.commands`))
+
+
+
+const embed = new EmbedBuilder()
+.setTitle("Botun Davet Linki")
+.setDescription(`[Botun Davet Etmek İçin Tıkla](https://discord.com/api/oauth2/authorize?client_id=1171024619466137700&permissions=8&scope=bot)**\n`)
+.setColor("#0099ff")
+.setTimestamp()
+.setThumbnail(client.user.displayAvatarURL({ dynamic: true, size: 1024 }))
+return message.reply({ embeds: [embed], components: [link_button] })
+
+}
+module.exports.conf = {
+aliases: ['davet1'],
+permLevel: 0, 
+kategori: 'Genel'
+};
+
+module.exports.help = {
+    name: 'davet1',
+    description: 'Bot davet linki atar.',
+    usage: 'botdavet'
+};
